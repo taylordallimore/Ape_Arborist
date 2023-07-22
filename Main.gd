28b8065c372f7df0a344b7b4c5d84f7dd7428419
dallimtayl <dallimtayl@ecs.vuw.ac.nz>
2023-07-22 14:49:14 +1200

    shoot on click test algo implimented

--- Main.gd
+++ Main.gd
@@ -3,6 +3,12 @@ extends Node2D
 func _on_start_pressed():
 	get_tree().change_scene_to_file("res://level_1.tscn")
 
+func _on_test_pressed():
+	print("Test")
+	#get_tree().change_scene_to_file("res://level_1.tscn")
+
+	get_tree().change_scene_to_file("res://test_scene.tscn")
+
 func _on_quit_pressed():
 	get_tree().quit()
 	print("Bolo")
