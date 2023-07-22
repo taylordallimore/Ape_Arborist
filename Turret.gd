c4be354fc08bf9e8b2cb491e7aa22440b042f607
dallimtayl <dallimtayl@ecs.vuw.ac.nz>
2023-07-22 19:52:53 +1200

    BANANA PHYSICS WORKING

--- Turret.gd
+++ Turret.gd
@@ -1,22 +1,26 @@
 extends Node2D
- 
 @onready var ray_cast = $RayCast2D
 @onready var timer = $Timer
 @export var ammo : PackedScene
+@onready var animation = get_node("AnimationPlayer")
+
 var can_fire = true
 var player
- 
+
 func _ready():
 	player = get_parent().find_child("player")
+	
 
 func _physics_process(_delta):
+	
 	_aim() #aim at player
+	
 	if Input.is_action_pressed("ui_accept"):
 		if can_fire:
+			animation.play("Throw")
 			_shoot()
-
-
-
+			
+		
 # func _input(event):
 # 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 # 	# Get the current mouse position relative to the global coordinate system
@@ -30,7 +34,8 @@ func _aim():
 func _on_timer_timeout():
 	can_fire = true
 
- 
+
+
 func _shoot():
 	var bullet = ammo.instantiate()
 	bullet.position = position
@@ -38,3 +43,4 @@ func _shoot():
 	get_tree().current_scene.add_child(bullet)
 	can_fire = false
 	timer.start()
+	
